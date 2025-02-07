1ca5ac674f024a349c970710cf558f5d71148981
Armoni Tigner <atigner81@gmail.com>
2025-02-07 14:37:12 -0500

    fixed styling for loaded data

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -13,35 +13,82 @@ import { get } from 'http';
 export class HeaderComponent {
   userInput: string = ''
   stockDetails:StockDetails[] = []
-  constructor( private stockService:StockDataService){}
+  constructor( private stockService:StockDataService ){}
 
   test(){
     // console.log(this.stockService.getstockData())
     console.log('hi click')
     console.log(this.userInput, 'input')
-  //   this.stockService.getstockData(this.userInput).subscribe((data:any) => {
- 
-  //     this.stockDetails.push(data.results)
-  //     console.log(this.stockDetails[0], 'ahhh')
-  //   //  let z = data.filter((obj: { Exchange: string; }) => obj.Exchange === "US")
-  //   //   this.stockDetails.push(data.filter((obj: { Exchange: string; }) => obj.Exchange === "US"))
-  //   //   console.log(this.stockDetails, 'lol')
-  //     // data
-  //   })
-  // }
-  this.stockService.getstockData(this.userInput).subscribe((data: any) => {
-    console.log(data)
-    const filterResponse = data.filter((obj: { Exchange: string; }) => obj.Exchange === "US" );
+  // this.stockService.getstockData(this.userInput).subscribe((data: any) => {
+    // console.log(data)
+    // const filterResponse = data.filter((obj: { Exchange: string; }) => obj.Exchange === "US" );
+    const filterResponse = [
+      {
+          "Code": "SPY",
+          "Exchange": "US",
+          "Name": "SPDR S&P 500 ETF Trust",
+          "Type": "ETF",
+          "Country": "USA",
+          "Currency": "USD",
+          "ISIN": "US78462F1030",
+          "previousClose": 606.32,
+          "previousCloseDate": "2025-02-06"
+      },
+      {
+          "Code": "SPY",
+          "Exchange": "BA",
+          "Name": "Caja de Valores S.A.",
+          "Type": "Common Stock",
+          "Country": "Argentina",
+          "Currency": "ARS",
+          "ISIN": null,
+          "previousClose": 36225,
+          "previousCloseDate": "2025-02-06"
+      },
+      {
+          "Code": "SPY",
+          "Exchange": "MX",
+          "Name": "SPDR S&P 500 ETF Trust",
+          "Type": "ETF",
+          "Country": "Mexico",
+          "Currency": "MXN",
+          "ISIN": "US78462F1030",
+          "previousClose": 12402.2598,
+          "previousCloseDate": "2025-02-06"
+      },
+      {
+          "Code": "SPY",
+          "Exchange": "AU",
+          "Name": "SPDR® S&P 500® ETF Trust",
+          "Type": "ETF",
+          "Country": "Australia",
+          "Currency": "AUD",
+          "ISIN": "AU000000SPY3",
+          "previousClose": 964.78,
+          "previousCloseDate": "2025-02-07"
+      },
+      {
+        "Code": "MONI",
+        "Exchange": "AU",
+        "Name": "SPDR® S&P 500® ETF Trust",
+        "Type": "ETF",
+        "Country": "Australia",
+        "Currency": "AUD",
+        "ISIN": "AU000000SPY3",
+        "previousClose": 964.78,
+        "previousCloseDate": "2025-02-07"
+    }
+  ]
     const getTickersForVolAndChange = filterResponse.map((val: {Code:string}) => val.Code)
-    const metaData = this.stockService.getVolAndChangeData(getTickersForVolAndChange)
+    // const metaData = this.stockService.getVolAndChangeData(getTickersForVolAndChange)
 
     
-    console.log(filterResponse, 'filterresponse')
-    console.log(metaData, 'metaData')
+    // console.log(filterResponse, 'filterresponse')
+    // console.log(metaData, 'metaData')
     this.stockDetails = []
 
     if(filterResponse.length >= 1){
-      for(let i = 0; i <= filterResponse.length ; i++){
+      for(let i = 0; i < filterResponse.length ; i++){
         console.log(i,'i')
         let details: StockDetails = {
           ticker: filterResponse[i].Code,
@@ -53,9 +100,16 @@ export class HeaderComponent {
   
          this.stockDetails.push(details)
         }
-        console.log('tick')
+    
 
-    }
+        // const toMap = new Map(filterResponse.map( (obj: {Code:string}) => [obj.Code, obj]))
+        // console.log(toMap.get('AAPL'), 'hereeeeee')
+        // // console.log(metaData, 'meta')
+        // const combinedData = metaData.map((obj: {code:string}) => (
+        //  console.log(obj, 'mick checl')
+        // ));
+        // console.log(combinedData, 'yurr')
+      }
 
     let details: StockDetails = {
       ticker: filterResponse[0].Code,
@@ -66,11 +120,8 @@ export class HeaderComponent {
      }
 
      this.stockDetails.push(details)
-     for( let obj of filterResponse){
-        console.log(obj, 'need YOO??')
-     }
-    
-  });
+
+ 
 }
 
 }
--- src/app/stockService/stock-data.service.ts
+++ src/app/stockService/stock-data.service.ts
@@ -9,7 +9,7 @@ export class StockDataService {
   constructor(private http:HttpClient) { }
 
   getstockData(input:string){
-   return this.http.get<any[]>(`https://eodhd.com/api/search/${input}?api_token=67908244556db6.70380137&fmt=json`)
+   return this.http.get<any[]>(`https://eodhd.com/api/search/${input}?api_token=67a12da01ba898.08534747&fmt=json`)
   //  https://financialmodelingprep.com/api/v3/search?query=Aapl&apikey=azGUw2JD3uNDIyNre5sF7Agsfnd45sNT&exchange=NASDAQ
   // https://api.polygon.io/v3/reference/tickers/AAPL?apiKey=zQvZsoJrKnEYXvd2Fj2O1zdOUp8ZuC1B
   }
@@ -18,7 +18,7 @@ export class StockDataService {
 
     for(let i = 0 ; i < arr.length ; i++){
       // let obj:{code:string,change:number, volume:number} ={code:'', change:0, volume: 0}
-      this.http.get<{code: string, change: number, change_p:number, volume:number}>(`https://eodhd.com/api/real-time/${arr[i]}.US?api_token=67908244556db6.70380137&fmt=json`).subscribe((data) => {
+      this.http.get<{code: string, change: number, change_p:number, volume:number}>(`https://eodhd.com/api/real-time/${arr[i]}.US?api_token=67a12da01ba898.08534747&fmt=json`).subscribe((data) => {
         console.log(data.code, 'INYO')
       if(data){
         let removeExtraWords = data.code.split('.')[0]
@@ -32,6 +32,7 @@ export class StockDataService {
       }
       })
     }
+    // dataObj.splice(0,1)
     return dataObj
 }
 
