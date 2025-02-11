3405251f2917ff0eecf1fb1dec8c9267906bd2cc
Armoni Tigner <atigner81@gmail.com>
2025-02-10 19:58:42 -0500

    added styling and color for change in price also the logic

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,13 +3,14 @@ import { StockDataService } from '../stockService/stock-data.service';
 import { StockDetails } from '../models/stock-details';
 import { OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-header',
   standalone: false,
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent implements OnInit{
+export class HeaderComponent implements OnInit {
 
 
   userInput: string = ''
@@ -20,14 +21,14 @@ export class HeaderComponent implements OnInit{
     this.stockDetails = this.stockService.getPopularStocks()
   }
 
-  test(){
+  async test()  {
     this.displayStockCount  = true
     // console.log(this.stockService.getstockData())
     console.log('hi click')
     console.log(this.userInput, 'input')
   // this.stockService.getstockData(this.userInput).subscribe((data: any) => {
     // console.log(data)
-    // const filterResponse = data.filter((obj: { Exchange: string; }) => obj.Exchange === "US" );
+    
     const filterResponse = [
       {
           "Code": "SPY",
@@ -41,8 +42,8 @@ export class HeaderComponent implements OnInit{
           "previousCloseDate": "2025-02-06"
       },
       {
-          "Code": "SPY",
-          "Exchange": "BA",
+          "Code": "TSLA",
+          "Exchange": "US",
           "Name": "Caja de Valores S.A.",
           "Type": "Common Stock",
           "Country": "Argentina",
@@ -85,31 +86,37 @@ export class HeaderComponent implements OnInit{
         "previousCloseDate": "2025-02-07"
     }
   ]
-    const getTickersForVolAndChange = filterResponse.map((val: {Code:string}) => val.Code)
+  const changeMeBack = filterResponse.filter((obj: { Exchange: string; }) => obj.Exchange === "US" );
+    const getTickersForVolAndChange = changeMeBack.map((val: {Code:string}) => val.Code)
+    console.log(getTickersForVolAndChange, 'tick')
     // const metaData = this.stockService.getVolAndChangeData(getTickersForVolAndChange)
+    // this.get.http<any[]>(`https://api.marketstack.com/v1/eod?access_key=09f210aa2592d9825bc793c70890e24e&symbols=${getTickersForVolAndChange[i]}`)
 
     
-    // console.log(filterResponse, 'filterresponse')
+    // console.log(changeMeBack, 'changeMeBack')
     // console.log(metaData, 'metaData')
     this.stockDetails = []
+    
+const changePrice = await this.stockService.getVolAndChangeData(getTickersForVolAndChange)
 
-    if(filterResponse.length >= 1){
-      for(let i = 0; i < filterResponse.length ; i++){
+    if(changeMeBack.length >= 1){
+      for(let i = 0; i <= changeMeBack.length ; i++){
         console.log(i,'i')
+        // this.stockService.getVolAndChangeData(getTickersForVolAndChange)
         let details: StockDetails = {
-          ticker: filterResponse[i].Code,
-          company: filterResponse[i].Name,
-          typeStock:filterResponse[i].Type,
-          prevClose: filterResponse[i].previousClose,
-          lastCloseDate: filterResponse[i].previousCloseDate,
-          changePrice: 0
+          ticker: changeMeBack[i].Code,
+          company: changeMeBack[i].Name,
+          typeStock:changeMeBack[i].Type,
+          prevClose: changeMeBack[i].previousClose,
+          lastCloseDate: changeMeBack[i].previousCloseDate,
+          changePrice: '-0.99'
          }
-  
+         console.log(details, 'detail')
          this.stockDetails.push(details)
         }
     
 
-        // const toMap = new Map(filterResponse.map( (obj: {Code:string}) => [obj.Code, obj]))
+        // const toMap = new Map(changeMeBack.map( (obj: {Code:string}) => [obj.Code, obj]))
         // console.log(toMap.get('AAPL'), 'hereeeeee')
         // // console.log(metaData, 'meta')
         // const combinedData = metaData.map((obj: {code:string}) => (
@@ -119,11 +126,11 @@ export class HeaderComponent implements OnInit{
       }
 
     let details: StockDetails = {
-      ticker: filterResponse[0].Code,
-      company: filterResponse[0].Name,
-      typeStock: filterResponse[0].Type,
-      prevClose: filterResponse[0].previousClose,
-      lastCloseDate: filterResponse[0].previousCloseDate,
+      ticker: changeMeBack[0].Code,
+      company: changeMeBack[0].Name,
+      typeStock: changeMeBack[0].Type,
+      prevClose: changeMeBack[0].previousClose,
+      lastCloseDate: changeMeBack[0].previousCloseDate,
       changePrice: 0
      }
 
--- src/app/stockService/stock-data.service.ts
+++ src/app/stockService/stock-data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { StockDetails } from '../models/stock-details';
+import { firstValueFrom } from 'rxjs'; //use this instead of toPromise() as it is now depricated
 @Injectable({
   providedIn: 'root'
 })
@@ -14,28 +15,31 @@ export class StockDataService {
   //  https://financialmodelingprep.com/api/v3/search?query=Aapl&apikey=azGUw2JD3uNDIyNre5sF7Agsfnd45sNT&exchange=NASDAQ
   // https://api.polygon.io/v3/reference/tickers/AAPL?apiKey=zQvZsoJrKnEYXvd2Fj2O1zdOUp8ZuC1B
   }
-  getVolAndChangeData(arr: []){
-    let dataObj:[{code:string,change:number, change_p:number,volume:number}] = [{code:'',change:0, change_p: 0,volume:0}]
-
-    for(let i = 0 ; i < arr.length ; i++){
-      // let obj:{code:string,change:number, volume:number} ={code:'', change:0, volume: 0}
-      this.http.get<{code: string, change: number, change_p:number, volume:number}>(`https://eodhd.com/api/real-time/${arr[i]}.US?api_token=67a12da01ba898.08534747&fmt=json`).subscribe((data) => {
-        console.log(data.code, 'INYO')
-      if(data){
-        let removeExtraWords = data.code.split('.')[0]
-        let obj:{code:string,change:number, change_p:number, volume:number} = {
-          code:removeExtraWords,
-          change: data.change,
-          change_p: data.change_p,
-          volume:data.volume
-        }
-        dataObj.push(obj)
-      }
-      })
-    }
-    // dataObj.splice(0,1)
-    return dataObj
-}
+  getVolAndChangeData(arr: string[]): Promise<(number | string)[]> {
+    console.log('Fetching stock data for:', arr);
+  
+    const requests = arr.map((symbol) =>
+      this.http
+        .get<any>(`https://api.marketstack.com/v1/eod?access_key=09f210aa2592d9825bc793c70890e24e&symbols=${symbol}`)
+        .toPromise()
+        .then((res) => {
+          if (res?.data?.[0]) {
+            const changeInPrice = res.data[0].open - res.data[0].close;
+            console.log(`Change for ${symbol}:`, changeInPrice);
+            return changeInPrice;
+          }
+          return '0'; // Default if data is missing
+        })
+        .catch((err) => {
+          console.error(`Error fetching ${symbol}:`, err);
+          return '0'; // Handle errors gracefully
+        })
+    );
+  
+    return Promise.all(requests);
+  }
+  
+  
 
 getPopularStocks(){
 const popularStocks = ['TSLA','AAPL', 'SPY', 'GOOG', 'AMZN', 'GME', 'MSFT']
@@ -43,6 +47,7 @@ const stockNames = ['Tesla', 'Apple', 'S&P 500', 'Google', 'Amazon', 'Microsoft'
 
 for(let i = 0; i < popularStocks.length; i++){
   this.http.get<{c:number | string, d:number}>(`https://finnhub.io/api/v1/quote?symbol=${popularStocks[i]}&token=cu88dlpr01qhqu5ccfk0cu88dlpr01qhqu5ccfkg`).subscribe((data) => {
+    console.log(data, 'OMAHHH')
     if(data){
       console.log(data)
       let details: StockDetails = {
